c17b3a72ca3ec6f08f8adbdccdcb60f55104caf8
reggiecheston <99285925+reggiecheston@users.noreply.github.com>
2024-01-09 16:51:09 -0500

    prints saved exercises to profile home

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 const workoutForm = document.querySelector("form");
 const searchResults = document.getElementById("search-results");
 const pageTitle = document.querySelector("h1");
-const ctaBtn = document.querySelectorAll(".cta-btn");
 
 const apiKey = "qAo4gHEpxVCdAlceVArlxA==f5E6owQ6vtyXpRio";
 
@@ -49,45 +48,42 @@ workoutForm.addEventListener("submit", async (e) => {
       searchResults.insertAdjacentHTML(
         "beforeend",
         `<div class="search-result">
-          <div class="scene scene--card">
-            <div class="card">
-              <div class="card__face card__face--front">
-                <div class="exercise-details">
+            <div class="exercise-details">
                 <h4 class="exercise-name">${e.name}</h4>
                 <small>${e.difficulty}</small>
+                <p class="exercise-instructions">${e.instructions}</p>
                 <div class="exercise-details__btns">
                     <button class="exercise-details__btn">How-To</button>
                     <button class="exercise-details__btn cta-btn" data-exercise-id="${e.muscle}" data-name="${e.name}">Save</button>
                 </div>
-              </div>
-              </div>
-              <div class="card__face card__face--back">
-              <p class="exercise-instructions">${e.instructions}</p>
             </div>
-            </div>
-          </div>
         </div>`
       )
     );
   }
+  const ctaBtn = document.querySelectorAll(".cta-btn");
+  const userID = document.getElementById("user-id").dataset.value;
+  console.log(userID);
+  console.log(ctaBtn);
+
+  ctaBtn.forEach((button) =>
+    button.addEventListener("click", async () => {
+      const exerciseName = button.dataset.name;
+      console.log(exerciseName);
+      console.log("button clicked");
+      const response = await fetch("/add-exercise", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userID: userID,
+          exercise_name: exerciseName,
+        }),
+      });
+
+      const data = await response.json();
+      console.log(data);
+    })
+  );
 });
-
-ctaBtn.forEach((button) =>
-  button.addEventListener("click", async () => {
-    const exerciseName = button.getAttribute("data-name");
-
-    const response = await fetch("/add-exercise", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        userID: userID,
-        exercise_name: exerciseName,
-      }),
-    });
-
-    const data = await response.json();
-    console.log(data);
-  })
-);
